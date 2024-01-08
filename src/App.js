9f1e2d99853b36079eb67ebd4624d7b257a97b35
Gikai Andrews <gnaboys@gmail.com>
2024-01-07 23:45:18 -0500

    added espprts logo for web tab and added navbar/homepage component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Homepage/>}/>
+
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
