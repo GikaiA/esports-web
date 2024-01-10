2ecffabfd95fd58e8361551a7dbbc786b639993d
Gikai Andrews <gnaboys@gmail.com>
2024-01-09 21:16:47 -0500

    fixed dropdown menu

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,47 +1,67 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
-const  Navbar = () => {
+const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const toggleMobileMenu =() => {
+  const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
   return (
-     <nav className="navbar">
-    <div
-      className='menu-toggle' onClick={toggleMobileMenu}
-    >
-      <div className="bar"></div>
-      <div className="bar"></div>
-      <div className="bar"></div>
-    </div>
-    <div className={`navbar-links ${mobileMenuOpen ? 'active' : ''}`}>
-      <div className="navbar-title">
-        <a href="/" className="navbar-title-link">Home</a>
+    <nav className="navbar">
+      <div className="menu-toggle" onClick={toggleMobileMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
-      <div className="navbar-title"> 
-        <a href="/aboutus" className="navbar-title-link">About Us</a>
-      </div>
-      <div className="navbar-title">
-        <a href="" className="navbar-title-link">Teams</a>
-        <div className="dropdown">
-        <a href="/teams/call-of-duty">Call of Duty</a> 
-          <a href="/teams/overwatch">Overwatch</a>  
-          <a href="/teams/league-of-legends">League of Legends</a>
-          <a href="/teams/csgo">CSGO</a>
-          <a href="/teams/valorant">Valorant</a>
-          <a href="/teams/rocket-league">Rocket League</a>
-          <a href="/teams/smash">Smash Bros</a>
+      <div className={`navbar-links ${mobileMenuOpen ? "active" : ""}`}>
+        <div className="navbar-title">
+          <a href="/" className="navbar-title-link">
+            Home
+          </a>
+        </div>
+        <div className="navbar-title">
+          <a href="/aboutus" className="navbar-title-link">
+            About Us
+          </a>
+        </div>
+        <div className="navbar-title">
+          <a href="" className="navbar-title-link">
+            Teams
+          </a>
+          <div className="dropdown">
+            <a href="/teams/call-of-duty" className="dropdown-title">
+              Call of Duty
+            </a>
+            <a href="/teams/overwatch" className="dropdown-title">
+              Overwatch
+            </a>
+            <a href="/teams/league-of-legends" className="dropdown-title">
+              League of Legends
+            </a>
+            <a href="/teams/csgo" className="dropdown-title">
+              CSGO
+            </a>
+            <a href="/teams/valorant" className="dropdown-title">
+              Valorant
+            </a>
+            <a href="/teams/rocket-league" className="dropdown-title">
+              Rocket League
+            </a>
+            <a href="/teams/smash" className="dropdown-title">
+              Smash Bros
+            </a>
+          </div>
+        </div>
+        <div className="navbar-title">
+          <a href="#skills" className="navbar-title-link">
+            Socials
+          </a>
         </div>
       </div>
-      <div className="navbar-title">
-       <a href="#skills" className="navbar-title-link">Socials</a>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
 };
 
 export default Navbar;
