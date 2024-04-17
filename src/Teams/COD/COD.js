7d606a373bd267f78304354373675d2c4e6349e6
Gikai Andrews <gnaboys@gmail.com>
2024-04-17 11:15:33 -0400

    moved some stuff and restarted cod component

--- src/Teams/COD/COD.js
+++ src/Teams/COD/COD.js
@@ -1,29 +1,18 @@
 import React from "react";
 import "./COD.css";
 import codlogo from "../COD/COD Logo.png";
+import codhero from '../COD/MW3-hero.jpg'
 
 function COD() {
   return (
-    <div className="cod-wrapper">
-      <div className="cod-parallax-bg"></div>
-      <div className="cod-section">
-        <img src={codlogo} alt="cod-logo" className="logo"></img>
+    <div className="cod-section">
+      <div className="hero">
+       <img src={codhero} alt="cod-hero" className="cod-image"></img> 
+       <div className="logo">
+        <img src={codlogo} alt="cod-logo" className="cod-logo"></img>
+       </div>
       </div>
-      <div className="cod-paragraph">
-        <p className="cod-text">
-          {" "}
-          Our Call of Duty team, led by Kaiju since 2020, experienced a
-          rollercoaster journey. Starting with Warzone battles, we transitioned
-          to Multiplayer with Black Ops Cold War, facing challenges. In 2022,
-          Vanguard's release shifted focus to multiplayer, overcoming chemistry
-          issues. In 2023, with a new team and manager Trex, we proudly secured
-          4th place in Spring. Now, aiming for 1st in Fall 2023 before Modern
-          Warfare III, this revitalized team is determined for success.
-        </p>
-        <div className="cod-roster">
-          <h1 className="cod-roster-title">Main Roster</h1>
-          <p className="roster-paragraph">Roster TBA</p>
-        </div>
+      <div className="roster-section">
       </div>
     </div>
   );
