d7465bd27dda5aacb5e3476128b1615ab4f150d4
Gikai Andrews <gnaboys@gmail.com>
2024-05-07 02:10:09 -0400

    added admin section on about page

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./About.css";
 import owlslogo from "../images/owls-esports.png";
+import kerry from '../images/kerry.jpg';
+import deandre from '../images/deandre.jpg';
+import ian from '../images/ian.jpg';
+import enrique from '../images/enrique.jpg';
+import zechariah from '../images/zechariah.jpg';
+import edwin from '../images/edwin.jpg';
 
 function About() {
   return (
@@ -19,6 +25,53 @@ function About() {
         Council, successfully obtaining approval and official recognition as a
         sports club.
       </p>
+      <div className="admin-section">
+        <h1 className="admin-title">Meet the Club Admin</h1>
+        <div className="card-grid">
+          <div className="card">
+            <img src={kerry} alt="admin-pfp" className="admin-pfp"></img>
+            <div className="admin-info">
+              <p className="admin-name">Kerry Mock</p>
+              <p className="admin-status">President</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={deandre} alt="admin-pfp" className="admin-pfp"></img>
+            <div className="admin-info">
+              <p className="admin-name">Deandre James</p>
+              <p className="admin-status">Vice President</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={ian} alt="admin-pfp" className="admin-pfp"></img>
+            <div className="admin-info">
+              <p className="admin-name">Ian Clark</p>
+              <p className="admin-status">Treasurer</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={enrique} alt="admin-pfp" className="admin-pfp"></img>
+            <div className="admin-info">
+              <p className="admin-name">Enrique Conant</p>
+              <p className="admin-status">Risk Management Officer</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={zechariah} alt="admin-pfp" className="admin-pfp"></img>
+            <div className="admin-info">
+              <p className="admin-name">Zechariah Riddle</p>
+              <p className="admin-status">Secretary</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={edwin} alt="admin-pfp" className="admin-pfp"></img>
+            <div className="admin-info">
+              <p className="admin-name">Edwin Gomez</p>
+              <p className="admin-status">Outreach Officer</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
